39a9c2de46eadb5d5669310bdefa8e0b86c5f0b0
alex-ta <alexander.taubert@hs-furtwangen.de>
2015-01-14 14:00:52 +0100

    update

--- src/XML.java
+++ src/XML.java
@@ -17,11 +17,23 @@ import org.w3c.dom.ls.LSSerializer;
 
 
 public class XML {
+	/**
+	 * @author Alex
+	 * XML Klasse die die XML Logik einigermaßen kapselt
+	 * in ein XML Dokument das XML daten beinhalten kann 
+	 * out ein XML Dokument das XML daten beinhalten kann 
+	 * */
+	
 	private Document in,out;
 	private DocumentBuilder built = null;
     
 	
 	public XML(){
+		/**
+		 * Erstellen einer Document Builder Instance, bei der die
+		 * überprüfung angestellt wird. Zuletzt wird das out Dokument 
+		 * erzeugt, das später beschrieben werden kann.
+		 * */
 		DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
 		fac.setValidating(true);
 		try {
@@ -37,6 +49,11 @@ public class XML {
 	}
 	
 	public Document read(String path){
+		/***
+		 * Diese Methode liest eine XML Datei über
+		 * die Pfadangabe ein und speichert die Werte im
+		 * Dokument in.
+		 * */
 		try{
 			in = built.parse(new File(path));
 			} catch (Exception e){
@@ -46,20 +63,32 @@ public class XML {
 	}
 	
 	public Element addRoot(String tagName){
+		/**
+		 * schreibt ein Wurzelelement auf das out Dokument
+		 * */
 		Element e =out.createElement(tagName);
 		out.appendChild(e);
 		return e;
 	}
 	public Element addElement(Element parentname,String tagName){
+		/**
+		 * schreibt ein Element auf das out Dokument
+		 * */
 		Element e =out.createElement(tagName);
 		parentname.appendChild(e);
 		return e;
 	}
 	public void addAttribute(Element tagName,String Attname,String value){
+		/**
+		 * schreibt Attribut auf das out Dokument
+		 * */
 		tagName.setAttribute(Attname, value);
 	}
 	
 	public boolean write(String path){
+		/**
+		 * schreibt das out Dokument an die path Adresse
+		 * */
 		DOMImplementation imp = out.getImplementation();
 		DOMImplementationLS add= (DOMImplementationLS) imp.getFeature("LS", "3.0");
 		LSSerializer writer = add.createLSSerializer();
@@ -68,6 +97,14 @@ public class XML {
 	}
 	
 	public static void sysout (Document doc){
+		/**
+		 * gibt ein XML Document das eingelesen wurde auf der Console aus
+		 * dazu wird eine Liste aller Kinder eines Elements erstellt.
+		 * dazu wird die Ebene der Hirachie in der Variable level abgelegt.
+		 * die Kindelemente der Wurzel werden in der Methode abgearbeitet,
+		 * haben diese wieder Kind Elemente kommt Rekursion durch die Funktion
+		 * rek aufgerufen.
+		 * */
 		LinkedList<MappingObj> childes = new LinkedList<MappingObj>();
 		IntegerObj level = new IntegerObj(0);
 		childes.add(new MappingObj(doc.getDocumentElement(),level.value));
@@ -78,7 +115,6 @@ public class XML {
 			childes.add(new MappingObj(n,level.value));	
 			rek(ls.item(i),childes,level);
 		}
-		
 		
 		for(MappingObj m : childes){
 			if(m.node.getNodeName().equals("#text"))
@@ -97,6 +133,9 @@ public class XML {
 	}
 	
 	private static void rek(Node n,List<MappingObj> list, IntegerObj level){	
+		/**
+		 * sammelt alle Kindelemente des Elements
+		 * */
 			NodeList ls = n.getChildNodes();
 			level.inc();
 			for(int i =0; i<ls.getLength();i++){
@@ -108,6 +147,9 @@ public class XML {
 }
 
 	class MappingObj{
+		/**
+		 * Objekt das ein Element und desen Hierachieebene speichert
+		 * */
 		public Node node;
 		public int level;
 		public MappingObj(Node item, int level) {
@@ -117,6 +159,7 @@ public class XML {
 	}
 	
 	class IntegerObj{
+		/**Integer per Referenz*/
 		int value;
 		public IntegerObj(int value){
 			this.value = value;
